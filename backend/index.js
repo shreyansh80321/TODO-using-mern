7e641a76240c5eda8d62634234ac16223f16021f
SHREYANSH80321 <SIBERSUPER1122@GMAIL.com>
2025-09-28 17:39:43 +0530

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -12,40 +12,19 @@ connectDB()
 
 const app = express()
 
+app.use(
+  cors({
+    origin: [
+      "https://frontend-mern-olive.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000
-
-app.get("/api/jokes", (req, res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: "A joke",
-      content: "This is a joke",
-    },
-    {
-      id: 2,
-      title: "A 2 joke",
-      content: "This is a joke 2",
-    },
-    {
-      id: 3,
-      title: "A 3 joke",
-      content: "This is a joke 3",
-    },
-    {
-      id: 4,
-      title: "A 4 joke",
-      content: "This is a joke 4",
-    },
-    {
-      id: 5,
-      title: "A 5 joke",
-      content: "This is a joke 5",
-    },
-  ];
-  res.send(jokes);
-});
 
 app.use("/api/todos", todoRoutes);
 
